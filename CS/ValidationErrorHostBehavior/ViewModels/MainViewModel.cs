c2f08cd7028f2273d0f4a2d24c54dbf12818edb2
Anthony Sam <anton.sameishchev@devexpress.com>
2021-09-17 15:10:16 +0300

    Update MainViewModel.cs

--- CS/ValidationErrorHostBehavior/ViewModels/MainViewModel.cs
+++ CS/ValidationErrorHostBehavior/ViewModels/MainViewModel.cs
@@ -2,43 +2,34 @@
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
-namespace ValidationErrorHostBehavior.ViewModels
-{
-    public class MainViewModel : ViewModelBase, IDataErrorInfo
-    {
+namespace ValidationErrorHostBehavior.ViewModels {
+    public class MainViewModel : ViewModelBase, IDataErrorInfo {
         protected string firstname_;
         [Required]
         [RegularExpression(@"^[a-zA-Z''-'\s]{1,40}$", 
          ErrorMessage = "Characters are not allowed.")]
-        public string FirstName
-        {
+        public string FirstName {
             get { return this.firstname_; }
             set { this.SetProperty(ref this.firstname_, value, "FirstName"); }
         }
-
 
         protected string lastname_;
         [Required]
         [RegularExpression(@"^[a-zA-Z''-'\s]{1,40}$", 
          ErrorMessage = "Characters are not allowed.")]
-        public string LastName
-        {
+        public string LastName {
             get { return this.lastname_; }
             set { this.SetProperty(ref this.lastname_, value, "LastName"); }
         }
 
-        public string Error
-        {
-            get
-            {
+        public string Error {
+            get {
                 return string.Join(";", this["FirstName"], this["LastName"]);
             }
         }
 
-        public string this[string columnName]
-        {
-            get
-            {
+        public string this[string columnName] {
+            get {
                 return IDataErrorInfoHelper.GetErrorText(this, columnName);
             }
         }
