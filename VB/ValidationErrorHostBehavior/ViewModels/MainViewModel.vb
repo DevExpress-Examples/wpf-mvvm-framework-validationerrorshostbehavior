9935b2d91ac7b20cef1f987931e8ac1487549618
DevExpressExampleBot <fakeEmail>
2026-01-31 00:23:23 +0400

    Source auto update [skip ci]

--- VB/ValidationErrorHostBehavior/ViewModels/MainViewModel.vb
+++ VB/ValidationErrorHostBehavior/ViewModels/MainViewModel.vb
@@ -1,46 +1,51 @@
-﻿Imports DevExpress.Mvvm
+Imports DevExpress.Mvvm
 Imports System.ComponentModel
 Imports System.ComponentModel.DataAnnotations
 
 Namespace ValidationErrorHostBehavior.ViewModels
-	Public Class MainViewModel
-		Inherits ViewModelBase
-		Implements IDataErrorInfo
 
-		Protected firstname_ As String
-		<Required>
-		<RegularExpression("^[a-zA-Z''-'\s]{1,40}$", ErrorMessage := "Characters are not allowed.")>
-		Public Property FirstName() As String
-			Get
-				Return Me.firstname_
-			End Get
-			Set(ByVal value As String)
-				Me.SetProperty(Me.firstname_, value, "FirstName")
-			End Set
-		End Property
+    Public Class MainViewModel
+        Inherits ViewModelBase
+        Implements IDataErrorInfo
 
-		Protected lastname_ As String
-		<Required>
-		<RegularExpression("^[a-zA-Z''-'\s]{1,40}$", ErrorMessage := "Characters are not allowed.")>
-		Public Property LastName() As String
-			Get
-				Return Me.lastname_
-			End Get
-			Set(ByVal value As String)
-				Me.SetProperty(Me.lastname_, value, "LastName")
-			End Set
-		End Property
+        Protected firstname_ As String
 
-		Public ReadOnly Property [Error]() As String Implements IDataErrorInfo.Error
-			Get
-				Return String.Join(";", Me("FirstName"), Me("LastName"))
-			End Get
-		End Property
+        <Required>
+        <RegularExpression("^[a-zA-Z''-'\s]{1,40}$", ErrorMessage:="Characters are not allowed.")>
+        Public Property FirstName As String
+            Get
+                Return firstname_
+            End Get
 
-		Default Public ReadOnly Property Item(ByVal columnName As String) As String Implements IDataErrorInfo.Item
-			Get
-				Return IDataErrorInfoHelper.GetErrorText(Me, columnName)
-			End Get
-		End Property
-	End Class
+            Set(ByVal value As String)
+                SetProperty(firstname_, value, "FirstName")
+            End Set
+        End Property
+
+        Protected lastname_ As String
+
+        <Required>
+        <RegularExpression("^[a-zA-Z''-'\s]{1,40}$", ErrorMessage:="Characters are not allowed.")>
+        Public Property LastName As String
+            Get
+                Return lastname_
+            End Get
+
+            Set(ByVal value As String)
+                SetProperty(lastname_, value, "LastName")
+            End Set
+        End Property
+
+        Public ReadOnly Property [Error] As String Implements IDataErrorInfo.[Error]
+            Get
+                Return String.Join(";", Me("FirstName"), Me("LastName"))
+            End Get
+        End Property
+
+        Default Public ReadOnly Property Item(ByVal columnName As String) As String Implements IDataErrorInfo.Item
+            Get
+                Return IDataErrorInfoHelper.GetErrorText(Me, columnName)
+            End Get
+        End Property
+    End Class
 End Namespace
